4169aa7da18f8881208266970d96855123fcb826
cvincmrt <cvincmrt@gmail.com>
2026-05-16 11:39:50 +0200

    upravujem vypis tabulky

--- views/admin/news_list.php
+++ views/admin/news_list.php
@@ -3,13 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>News list</title>
 </head>
 <body>
-    <?php if(isset($newsList)) :?>
-        <?php foreach($newsList as $novelty) :?>
-            <h2><?=$novelty->getContent(); ?></h2>
-        <?php endforeach; ?>
-    <?php endif; ?>
+    <div class="container my-3">
+        <h2 class="mb-3">News list</h2>
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Content</th>
+                    <th scope="col">Image_path</th>
+                    <th scope="col">Created_at</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if(isset($newsList)) :?>
+                    <?php foreach($newsList as $novelty) :?>
+                        <tr>
+                            <td><?= $novelty->getTitle(); ?></td>
+                            <td><?= $novelty->getContent(); ?></td>
+                            <td><?= $novelty->getImagePath(); ?></td>
+                            <td><?= $novelty->getCreatedAt(); ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php endif; ?> 
+            </tbody>
+        </table>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
